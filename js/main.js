a794a1e733300a3238f1c386f1323b850abe3679
Phpner <phpner@gmail.com>
2018-10-12 10:37:38 +0300

    block6

--- js/main.js
+++ js/main.js
@@ -32,15 +32,20 @@ jQuery(document).ready(function($) {
 		console.log($(window).innerWidth());
 	}
 
+	$('.image-popup').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
 	
 });
-
-function hovermy(e){
-		
-		$(this).click(function(event) {
-
-		
-			
-
-		});
-}
